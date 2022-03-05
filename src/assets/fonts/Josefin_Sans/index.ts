b13290ccd2d729b453cd8d7f987f24e3c9bccd4c
Emerson <emerson.sarza@toptal.com>
2022-03-05 20:01:29 +0800

    Set up logo, font style

---
+++ src/assets/fonts/Josefin_Sans/index.ts
@@ -0,0 +1,6 @@
+import './static/JosefinSans-Bold.ttf';
+import './static/JosefinSans-ExtraLight.ttf';
+import './static/JosefinSans-Light.ttf';
+import './static/JosefinSans-Medium.ttf';
+import './static/JosefinSans-Regular.ttf';
+import './static/JosefinSans-Thin.ttf';
